5118c1336de5a43d246fa1816e7c352cbc3794f5
Evgeniya81 <78889718+Evgeniya81@users.noreply.github.com>
2021-03-13 19:52:47 +0700

    Delete Selenium directory

--- Selenium/src/test/java/AShot.java
+++
@@ -1,6 +0,0 @@
-import org.openqa.selenium.WebDriver;
-
-public class AShot {
-    public void takeScreenshot(WebDriver driver) {
-    }
-}
--- Selenium/src/test/java/Screenshot.java
+++
@@ -1,7 +0,0 @@
-import java.awt.image.RenderedImage;
-
-public class Screenshot {
-    public RenderedImage getImage() {
-        return null;
-    }
-}
